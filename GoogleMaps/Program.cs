df81373cc2614a222c0cbb5f6f20d6aa90d7a73f
CRGBx <97006998+CRGBx@users.noreply.github.com>
2022-12-18 22:03:20 +0530

    Add project files.

---
+++ GoogleMaps/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.StaticFiles;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+var provider = new FileExtensionContentTypeProvider();
+// Add new mappings
+provider.Mappings[".kml"] = "application/vnd.google-earth.kml+xml";
+provider.Mappings[".gpx"] = "application/gpx+xml";
+app.UseStaticFiles(new StaticFileOptions { ContentTypeProvider = provider });
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
